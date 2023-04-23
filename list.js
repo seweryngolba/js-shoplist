9dd27da6d24cd1c860dfe9f14f0c3eb4432128c7
Seweryn Golba <sgoolb@gmail.com>
2023-04-23 20:05:50 +0200

    new products

---
+++ list.js
@@ -0,0 +1,28 @@
+const arrayContainer = document.querySelector("array-list");
+
+let shopProduct = [
+  {
+    productName: "Piłka",
+    productPrice: 59.99,
+  },
+  {
+    productName: "Pompka",
+    productPrice: 19.99,
+  },
+  {
+    productName: "Koszulka",
+    productPrice: 99.99,
+  },
+  {
+    productName: "Bluza",
+    productPrice: 125.99,
+  },
+  {
+    productName: "Torba",
+    productPrice: 159.99,
+  },
+  {
+    productName: "Buty",
+    productPrice: 239.99,
+  },
+];
